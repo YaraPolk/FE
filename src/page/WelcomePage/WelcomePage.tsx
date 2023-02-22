07c9e0b919b9ae8dbc8c7fc527b8b4033b9ac001
YaraPolk <yaroslav260493@gmail.com>
2023-02-22 17:52:35 +0300

    create Header module

--- src/page/WelcomePage/WelcomePage.tsx
+++ src/page/WelcomePage/WelcomePage.tsx
@@ -1,15 +1,26 @@
 import React from "react";
 import style from './WelcomePage.module.scss';
-class WelcomePage extends React.Component{
+class WelcomePage extends React.Component<any, {token: boolean}>{
+
+    token: boolean = false;
+
+    GetContent = () => {
+        if (!this.token) {
+            return (
+                <section className={style.welcome_page}>
+                    <div className="welcome-text">
+                        <h1>Welcome to Trello</h1>
+                        <h2>You need to sign up or sign in in order to continue</h2>
+                    </div>
+                </section>
+            );
+        } else {
+            window.location.href = "/main_page";
+        }
+    }
+
     render() {
-        return (
-            <section className={style.welcome_page}>
-                <div className="welcome-text">
-                    <h1>Welcome to Trello</h1>
-                    <h2>You need to sign up or sign in in order to continue</h2>
-                </div>
-            </section>
-        );
+        return (this.GetContent());
     }
 }
 
