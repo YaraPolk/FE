791ef31adf9a1c8f90f04a1dc456e4598f337b9a
YaraPolk <yaroslav260493@gmail.com>
2023-02-22 13:18:38 +0300

    Create project and add welcome page

---
+++ src/page/MainPage/MainPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class MainPage extends React.Component{
+    render() {
+        return (
+            <section className={'home-page'}>
+                <h1>This will be your Board List</h1>
+            </section>
+        );
+    }
+}
+
+export default MainPage;
